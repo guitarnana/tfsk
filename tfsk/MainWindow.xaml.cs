00b4b36bd7c0a147fa65acb093f31cc9e881c7d8
Jarupat Jisarojito <jarupatj@microsoft.com>
2016-01-20 16:46:38 -0800

    Handle case where we don't have previous version by not showing anything. Add commandline arguments

--- tfsk/MainWindow.xaml.cs
+++ tfsk/MainWindow.xaml.cs
@@ -48,56 +48,103 @@ namespace tfsk
 		{
 			InitializeComponent();
 
-			var tfsUrl = "http://sqlbuvsts01:8080/main";
-			var serverPath = @"$/Developer/jarupatj";
-			//var serverPath = @"$/SQL Server/Imp/DS_Main";
+			Dictionary<string, string> arguments;
 
-			// Get a reference to our Team Foundation Server. 
+			if (!ParseCommandlineArguments(out arguments))
+			{
+				// print usage
+
+				return;
+			}
+
+			string tfsUrl, path;
+			if (!arguments.TryGetValue("server", out tfsUrl))
+			{
+				tfsUrl = "http://sqlbuvsts01:8080/main";
+				Console.WriteLine("No server specify. Default to http://sqlbuvsts01:8080/main");
+			}
+
+			if (!arguments.TryGetValue("path", out path))
+			{
+				path = @"$/SQL Server/Imp/DS_Main";
+				Console.WriteLine(@"No path specify. Default to $/SQL Server/Imp/DS_Main");
+			}
+			//var serverPath = @"$/Developer/jarupatj";
+			
 			TfsTeamProjectCollection tpc = new TfsTeamProjectCollection(new Uri(tfsUrl));
 
-			// Get a reference to Version Control. 
 			versionControl = tpc.GetService<VersionControlServer>();
 
-			List<Changeset> changesets = versionControl.QueryHistory(serverPath, RecursionType.Full, 100).ToList();
+			List<Changeset> changesets = versionControl.QueryHistory(path, RecursionType.Full, 100).ToList();
 
 			lvChangeset.ItemsSource = changesets;
 
 			UpdateUI(changesets[0]);
 		}
 
+		private bool ParseCommandlineArguments(out Dictionary<string, string> arguments)
+		{
+			bool success = true;
+			arguments = new Dictionary<string, string>();
+			string[] args = Environment.GetCommandLineArgs();
+			for (int i = 1; i < args.Length; i += 2 )
+			{
+				if (String.Equals(args[i], "-server", StringComparison.OrdinalIgnoreCase))
+				{
+					arguments.Add(args[i].Replace("-", ""), args[i+1]);
+				}
+				else if (String.Equals(args[i], "-path", StringComparison.OrdinalIgnoreCase))
+				{
+					arguments.Add(args[i].Replace("-", ""), args[i + 1]);
+				}
+				else
+				{
+					success = false;
+				}
+			}
+
+			return success;
+		}
+
 		private string DiffItemWithPrevVersion(VersionControlServer versionControl, Item item)
 		{
+			string diffStr = "";
+
 			// Get previous version item
 			//
 			Item prevItem = versionControl.GetItem(item.ItemId, item.ChangesetId - 1);
+			if (prevItem != null)
+			{
+				DiffItemVersionedFile curFile = new DiffItemVersionedFile(versionControl, item.ItemId, item.ChangesetId, null);
+				DiffItemVersionedFile prevFile = new DiffItemVersionedFile(versionControl, prevItem.ItemId, prevItem.ChangesetId, null);
 
-			DiffItemVersionedFile curFile = new DiffItemVersionedFile(versionControl, item.ItemId, item.ChangesetId, null);
-			DiffItemVersionedFile prevFile = new DiffItemVersionedFile(versionControl, prevItem.ItemId, prevItem.ChangesetId, null);
+				// Create memory stream for buffering diff output in memory
+				//
+				MemoryStream memStream = new MemoryStream();
 
-			// Create memory stream for buffering diff output in memory
-			//
-			MemoryStream memStream = new MemoryStream();
+				// Here we set up the options to show the diffs in the console with the unified diff 
+				// format.
+				DiffOptions options = new DiffOptions();
+				options.UseThirdPartyTool = false;
 
-			// Here we set up the options to show the diffs in the console with the unified diff 
-			// format.
-			DiffOptions options = new DiffOptions();
-			options.UseThirdPartyTool = false;
+				// These settings are just for the text diff (not needed for an external tool). 
+				options.Flags = DiffOptionFlags.EnablePreambleHandling | DiffOptionFlags.IgnoreWhiteSpace;
+				options.OutputType = DiffOutputType.Unified;
+				options.TargetEncoding = Console.OutputEncoding;
+				options.SourceEncoding = Console.OutputEncoding;
+				options.StreamWriter = new StreamWriter(memStream);
+				options.StreamWriter.AutoFlush = true;
 
-			// These settings are just for the text diff (not needed for an external tool). 
-			options.Flags = DiffOptionFlags.EnablePreambleHandling | DiffOptionFlags.IgnoreWhiteSpace;
-			options.OutputType = DiffOutputType.Unified;
-			options.TargetEncoding = Console.OutputEncoding;
-			options.SourceEncoding = Console.OutputEncoding;
-			options.StreamWriter = new StreamWriter(memStream);
-			options.StreamWriter.AutoFlush = true;
+				Difference.DiffFiles(versionControl, prevFile, curFile, options, prevItem.ServerItem, true);
 
-			Difference.DiffFiles(versionControl, prevFile, curFile, options, prevItem.ServerItem, true);
+				// Move to the beginning of the stream for reading.
+				memStream.Seek(0, SeekOrigin.Begin);
 
-			// Move to the beginning of the stream for reading.
-			memStream.Seek(0, SeekOrigin.Begin);
+				StreamReader sr = new StreamReader(memStream);
+				diffStr = sr.ReadToEnd();
+			}
 
-			StreamReader sr = new StreamReader(memStream);
-			return sr.ReadToEnd();
+			return diffStr;
 		}
 
 		private void lvChangeset_SelectionChanged(object sender, SelectionChangedEventArgs e)
