b6f57eb5a58e30e5c5ae7b81c6cebd8283d47d93
Jarupat Jisarojito <jarupatj@microsoft.com>
2016-02-07 16:34:35 -0800

    Add CommandlineArgument class

---
+++ tfsk/CommandlineArgument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tfsk
+{
+	public class CommandlineArgument
+	{
+		public string FilePath { get; set; }
+		public int NumDisplay { get; set; }
+		public string[] ExcludeUsers { get; set; }
+		public string VersionMin { get; set; }
+		public string VersionMax { get; set; }
+		public bool GetLatestVersion { get; set; }
+		public bool NoMinVersion { get; set; }
+
+		public CommandlineArgument()
+		{
+			NumDisplay = 100;
+			NoMinVersion = true;
+			GetLatestVersion = true;
+		}
+
+	}
+}
